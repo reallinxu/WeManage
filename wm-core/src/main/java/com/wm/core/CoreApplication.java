70588d62d071aece01d3651aff608d75c93b112d
linxu <linxu@csii.com.cn>
2020-07-01 20:21:03 +0800

    init

--- wm-core/src/main/java/com/wm/core/CoreApplication.java
+++ wm-core/src/main/java/com/wm/core/CoreApplication.java
@@ -2,15 +2,12 @@ package com.wm.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class CoreApplication {
 
-    @RequestMapping("/")
-    public String home() {
-        return "Hello World";
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(CoreApplication.class, args);
