a0a1130ec14513c2116e47b695924f30f66c782a
linxu <linxu@csii.com.cn>
2020-07-02 19:33:05 +0800

    init

--- wm-core/src/main/java/com/wm/core/CoreApplication.java
+++ wm-core/src/main/java/com/wm/core/CoreApplication.java
@@ -2,15 +2,26 @@ package com.wm.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RefreshScope
+@EnableDiscoveryClient
 public class CoreApplication {
-
 
     public static void main(String[] args) {
         SpringApplication.run(CoreApplication.class, args);
     }
-
+    @RestController
+    class EchoController {
+        @RequestMapping(value = "/echo/{string}", method = RequestMethod.GET)
+        public String echo(@PathVariable String string) {
+            return "Hello Nacos Discovery " + string;
+        }
+    }
 }
